acfa3cb9fc2614a92ef859939eec56137790056d
Thiago Santos <tisantos.ext@vr.com.br>
2021-02-10 14:58:37 -0300

    feat: Adição do react router dom e as devidas páginas bases do projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 
-const App: React.FC = () => <div className="App">Hello World</div>;
+const App: React.FC = () => (
+  <Router>
+    <Routes />
+  </Router>
+);
 
 export default App;
